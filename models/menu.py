9ab4222403c1d3504120bfc92cb32153d4391b38
jasndeye <73610141+jasndeye@users.noreply.github.com>
2022-02-17 11:28:39 +0000

    ajout des commentaires

--- models/menu.py
+++ models/menu.py
@@ -9,9 +9,9 @@ class menu(models.Model):
      nom=fields.Char(string="Nom du menu")
      produit_ids = fields.Many2many('commerciale.produit')
      date_menu = fields.Date(string="Date du menu", default=fields.Date.today)
-     
+     # controle de l'unicité sur la date du menu
      _sql_constraints = [ ('id_unique','UNIQUE(date_menu)', "On doit avoir un menu par jour")]
-     
+      # on force la date a etre la date d'aujord'hui
      @api.onchange('date_menu')
      def _Verifier_date_menu(self):
                if self.date_menu!=fields.Date.today():
